dd6758a6700877e7fb90eddfb760f3e97d9d0c59
Brandon Whitehead <tricksterguy@live.com>
2015-11-01 23:44:43 -0800

    Fixes, main.o had a bug, not neccessary to modify imageinfo.c

--- mode3/imageinfo.c
+++ mode3/imageinfo.c
@@ -1,4 +1,4 @@
-// Modify this file if you change the image used.
+// Modification not needed the image is resized to 240 by 160
 
 const int background_width = 240;
 const int background_height = 160;
