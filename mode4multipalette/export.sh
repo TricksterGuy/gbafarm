45c86c28626135bde6d19889aa4057a3e1d7f389
Brandon <tricksterguy@live.com>
2015-11-02 09:23:22 -0800

    Mode 4 multipalette demo open for business

--- mode4multipalette/export.sh
+++ mode4multipalette/export.sh
@@ -1,3 +1,3 @@
-nin10kit -mode=4 -palette=8 -start=92 -log=4 jigglypuff jigglypuff.gif
-nin10kit -mode=4 -palette=92 -start=0 -log=4 pikachu pikachu.png
-nin10kit -mode=4 -palette=156 -start=100 -log=4 dragonite dragonite.png 
+nin10kit -mode=4 -palette=92 -start=0 image1 image1.png
+nin10kit -mode=4 -palette=8 -start=92 image2 image2.gif
+nin10kit -mode=4 -palette=156 -start=100 image3 image3.png 
---
+++ mode4multipalette/imageinfo.c
@@ -0,0 +1,28 @@
+#include "image1.h"
+#include "image2.h"
+#include "image3.h"
+
+// pikachu
+const int image1_width = IMAGE1_WIDTH;
+const int image1_height = IMAGE1_HEIGHT;
+const int image1_palette_size = IMAGE1_PALETTE_SIZE;
+const int image1_palette_offset = 0;
+const int image1_x = 0;
+const int image1_y = 0;
+
+// jigglypuff
+const int image2_width = IMAGE2_WIDTH;
+const int image2_height = IMAGE2_HEIGHT;
+const int image2_palette_size = IMAGE2_PALETTE_SIZE;
+const int image2_palette_offset = IMAGE2_PALETTE_OFFSET;
+const int image2_x = 0;
+const int image2_y = 160 - 32;
+
+// dragonite
+const int image3_width = IMAGE3_WIDTH;
+const int image3_height = IMAGE3_HEIGHT;
+const int image3_palette_size = IMAGE3_PALETTE_SIZE;
+const int image3_palette_offset = IMAGE3_PALETTE_OFFSET;
+const int image3_x = (240 - 32 - 48) / 2;
+const int image3_y = (160 - 48) / 2;
+
