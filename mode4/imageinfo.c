d99fc0f8f7bd492793281e5553cfbfe449dacb42
Brandon <tricksterguy87@gmail.com>
2017-04-16 13:50:33 -0700

    Mode 4 demo open for business

--- mode4/imageinfo.c
+++
@@ -1,6 +0,0 @@
-#include "background.h"
-
-const int background_width = BACKGROUND_WIDTH;
-const int background_height = BACKGROUND_HEIGHT;
-const int background_palette_size = BACKGROUND_PALETTE_SIZE;
-
