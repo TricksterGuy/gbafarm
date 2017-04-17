fe7a009e96122a3cc9c653281afdc3318c0eeaf0
Brandon <TricksterGuy@users.noreply.github.com>
2017-04-17 01:21:23 -0700

    Update export.sh for nin10kit 1.7

--- sprite_demo_4bpp/export.sh
+++ sprite_demo_4bpp/export.sh
@@ -1,2 +1,2 @@
-nin10kit -mode=sprites -bpp=4 -export_2d=1 -transparent=FFFFFF spritedemo16 sprite64x64.png sprite64x32.png sprite32x64.png sprite32x32.png sprite32x16.png sprite32x8.png sprite16x32.png sprite16x16.png sprite16x8.png sprite8x32.png sprite8x16.png sprite8x8.png thing.png
-nin10kit -mode=0 -bpp=8 background background.bmp
+nin10kit --mode=sprites --bpp=4 --export_2d --transparent=FFFFFF spritedemo16 sprite64x64.png sprite64x32.png sprite32x64.png sprite32x32.png sprite32x16.png sprite32x8.png sprite16x32.png sprite16x16.png sprite16x8.png sprite8x32.png sprite8x16.png sprite8x8.png thing.png
+nin10kit --mode=0 --bpp=8 background background.bmp
