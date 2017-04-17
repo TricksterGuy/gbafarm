73fbaccff16dc20873ad4cac73bb567963913f31
Brandon <tricksterguy87@gmail.com>
2017-04-16 21:32:00 -0700

    Mode 4 Multi Palette Open for Business

--- mode4multipalette/export.sh
+++ mode4multipalette/export.sh
@@ -1,3 +1,2 @@
-nin10kit -mode=4 -palette=92 -start=0 image1 image1.png
-nin10kit -mode=4 -palette=8 -start=92 image2 image2.gif
-nin10kit -mode=4 -palette=156 -start=100 image3 image3.png 
+nin10kit --mode=4 --palette=92 --start=0 --resize=32x32 image1 image1.png
+nin10kit --mode=4 --palette=8 --start=92 --resize=240x160 background background.png
