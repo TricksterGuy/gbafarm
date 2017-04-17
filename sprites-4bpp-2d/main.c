3c79fd536da9dbf043a252b15f6ef3fc4402233e
Brandon <tricksterguy87@gmail.com>
2017-04-17 00:58:06 -0700

    Update demos to conform to _PALETTE to _PALETTE_ID change; Sprites 8 bpp 1D open for business

--- sprites-4bpp-2d/main.c
+++ sprites-4bpp-2d/main.c
@@ -23,17 +23,17 @@ int main(void)
     OamEntry* blue = shadow;
     blue->attr0 = 32 | SPRITES_PALETTE_TYPE | BLUE8X8_SPRITE_SHAPE;
     blue->attr1 = 64 | BLUE8X8_SPRITE_SIZE;
-    blue->attr2 = BLUE8X8_PALETTE | BLUE8X8_ID;
+    blue->attr2 = BLUE8X8_PALETTE_ID | BLUE8X8_ID;
 
     OamEntry* bowser = shadow + 1;
     bowser->attr0 = 19 | SPRITES_PALETTE_TYPE | BOWSER4X4_SPRITE_SHAPE;
     bowser->attr1 = 12 | BOWSER4X4_SPRITE_SIZE;
-    bowser->attr2 = BOWSER4X4_PALETTE | BOWSER4X4_ID;
+    bowser->attr2 = BOWSER4X4_PALETTE_ID | BOWSER4X4_ID;
 
     OamEntry* tiny = shadow + 2;
     tiny->attr0 = 100 | SPRITES_PALETTE_TYPE | TINY1X1_SPRITE_SHAPE;
     tiny->attr1 = 0 | TINY1X1_SPRITE_SIZE;
-    tiny->attr2 = TINY1X1_PALETTE | TINY1X1_ID;
+    tiny->attr2 = TINY1X1_PALETTE_ID | TINY1X1_ID;
 
     while(1)
     {
--- sprites-8bpp-1d/export.sh
+++ sprites-8bpp-1d/export.sh
@@ -1,1 +1,1 @@
-nin10kit -mode=sprites -log=4 sprites blue8x8.png burnt2x2.png tiny1x1.png
+nin10kit --mode=sprites --bpp=8 sprites blue8x8.png bowser4x4.png tiny1x1.png
