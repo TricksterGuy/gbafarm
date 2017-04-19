4e31cff09613425d568cef68a1a1e0f6961dc21c
Brandon <tricksterguy87@gmail.com>
2017-04-18 20:44:37 -0700

    Mode 0 4bpp tiles and map now open for business

--- mode0-4bpptilesandmap/export.sh
+++ mode0-4bpptilesandmap/export.sh
@@ -1,2 +1,2 @@
-nin10kit -mode=map -tileset=simple_tileset.png -bpp=4 gamemap gamemap.gif
-nin10kit -mode=tiles -bpp=4 simple simple_tileset.png
+nin10kit --mode=map --tileset_image=simple_tileset.png --bpp=4 gamemap gamemap.gif
+nin10kit --mode=tiles --bpp=4 simple simple_tileset.png
